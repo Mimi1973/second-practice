9499e88d4f249a2233fa7f4e1359aa0a75e64123
Mimi1973 <etse.haile2@gmail.com>
2022-05-06 23:00:19 -0400

    commit from Eclipse

---
+++ Gitpractice/src/com/practice/Fridaypractice.java
@@ -0,0 +1,11 @@
+package com.practice;
+
+public class Fridaypractice {
+
+	public static void main(String[] args) {
+				// TODO Auto-generated method stub
+		System.out.println ("Hello World");
+		System.out.println ("Hello Mom");
+	}
+
+}
